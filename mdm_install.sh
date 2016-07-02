745954c3c02a5a62e18809740e3606cb6e0252a6
Michael Marrotte <marrotte@gmail.com>
2016-07-02 10:37:31 -0400

    u2d scripts and added an SDC+filesystem to the MDM

--- mdm_install.sh
+++ mdm_install.sh
@@ -35,4 +35,27 @@ for sds_ip in `cat /tmp/all_sds`; do
   scli --add_sds --mdm_ip $MDM --sds_ip $sds_ip --device_path /dev/xvdb --sds_name $sds_ip --protection_domain_name pdomain --storage_pool_name pool1
 done
 
+echo "Installing SDC on MDM"
+MDM_IP=$MDM rpm -i https://scaleio-source.s3.amazonaws.com/1.32/EMC-ScaleIO-sdc-1.32-403.2.el7.x86_64.rpm
+
+sleep 5;
+
+echo "Creating volume vol01"
+scli --protection_domain_name pdomain --add_volume --storage_pool_name pool1 --size_gb 100 --volume_name vol01
+
+echo "Mapping vol01 to MDM"
+scli --map_volume_to_sdc --volume_name vol01 --sdc_ip $MDM
+
+sleep 5;
+
+echo "Creating partition on vol01"
+sudo parted -a optimal -- /dev/scinia mklabel gpt mkpart P1 ext4 "1" "-1"
+
+echo "Creating file system on volo1"
+sudo mkfs.ext4 /dev/scinia
+
+echo "Mounting file system"
+sudo mkdir /mnt_scinia
+sudo mount /dev/scinia /mnt_scinia
+
 EOF
